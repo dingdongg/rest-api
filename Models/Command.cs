393799b0dc28607912bc8e81cbfd98bc8526cabf
donggyu <donggyu258@gmail.com>
2022-05-03 22:16:55 -0700

    added a few properties of the Command class

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,4 +1,10 @@
 namespace APIProject.Models
 {
-    
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
 }
