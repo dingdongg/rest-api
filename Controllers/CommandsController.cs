2f661453e7da701ede20b3787129ee7a7a3c4b4c
donggyu <donggyu258@gmail.com>
2022-05-03 23:22:04 -0700

    rough version of the controller in the MVC model

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using APIProject.Data;
+using APIProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject.Controllers
+{
+    // no view support in this architecture, so use ControllerBase over Controller
+    [Route("api/commands")] // controller-level route
+    [ApiController] 
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
