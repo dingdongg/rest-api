0af377b9f35b9b248d894171c47fe7817e4687ac
donggyu <donggyu258@gmail.com>
2022-05-04 22:40:52 -0700

    controller correctly returns the corresponding DTO

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using APIProject.Data;
+using APIProject.DTOs;
 using APIProject.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIProject.Controllers
@@ -11,30 +13,32 @@ namespace APIProject.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
         // constructor to inject dependency
-        public CommandsController(ICommanderRepo repo)
+        public CommandsController(ICommanderRepo repo, IMapper mapper)
         {
             _repository = repo;
+            _mapper = mapper;
         }
 
         // GET api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDTO>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDTO>>(commandItems));
         }
 
         // GET api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id)
+        public ActionResult<CommandReadDTO> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
             if (commandItem != null)
             {
-                return Ok(commandItem);
+                return Ok(_mapper.Map<CommandReadDTO>(commandItem));
             }
             return NotFound();
         }
